50bde7839dab8026e5ddd696287e920e2c4d9153
FredrikAndre <fredrik.he.andre@gmail.com>
2021-02-14 12:28:37 +0100

    Added first EJS files and included  bootstrap

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Import functions
+const homeController = require("../controllers/homeController");
+
+router.get("/", homeController.showStart);
+
 module.exports = router;
