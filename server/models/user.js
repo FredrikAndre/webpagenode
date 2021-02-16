58fc747dbc265a9cdeae0dfd732ce0609a623fc1
FredrikAndre <fredrik.he.andre@gmail.com>
2021-02-16 15:26:30 +0100

    Added some new stuff in user.js

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,20 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   cryptoUrl: String,
   cryptoUrlExpiration: Date,
+  todoList: // New
+  [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Todo"
+  }]
 });
+
+userSchema.methods.addToList = function(todoItemId) { // New
+  // Pushar in i shoppingCart
+  this.todoList.push(todoItemId)
+  // Filtrera data så att användare inte ska kunna lägga till samma course två gånger
+  this.save();
+
+}
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
